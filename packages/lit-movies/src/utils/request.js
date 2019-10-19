b258e72b4f152359ff85d61ec1a76306308c6f76
Hardik Pithva <hardikpthv@gmail.com>
2019-10-19 13:38:01 +0200

    feat: #UseThePlatform

--- packages/lit-movies/src/utils/request.js
+++ packages/lit-movies/src/utils/request.js
@@ -1,18 +1,16 @@
-import axios from 'axios';
 /**
  * Request service for network calls.
  */
 export default {
-    /**
-     * Get request
-     *
-     * @param {string} url - network request url
-     * @param {Object} request - request configuration
-     */
-    get(url, request) {
-        return axios
-            .get(url, request)
-            .then(resp => Promise.resolve(resp.data))
-            .catch(err => Promise.reject(err));
-    },
+  /**
+   * Get request
+   *
+   * @param {string} url - network request url
+   * @param {Object} request - request configuration
+   */
+  get(url, request) {
+    return fetch(url, request)
+      .then(resp => Promise.resolve(resp.json()))
+      .catch(err => Promise.reject(err));
+  },
 };
